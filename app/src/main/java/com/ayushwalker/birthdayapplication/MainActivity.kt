dafffa8309cba957d63aca802e0cc261b66b9cba
Nitesh Kumar <niteshman30@gmail.com>
2022-07-26 08:51:26 +0530

    data binding added

--- app/src/main/java/com/ayushwalker/birthdayapplication/MainActivity.kt
+++ app/src/main/java/com/ayushwalker/birthdayapplication/MainActivity.kt
@@ -4,15 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.ayushwalker.birthdayapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       binding=DataBindingUtil. setContentView(this,R.layout.activity_main)
+
+        //--you can also use this below code same functionality--
+
+//        with(binding){
+//            createBirthdayButton.setOnClickListener {
+//                val name = binding.editTextPersonName.editableText.toString()
+//                Toast.makeText(this, "Name is $name" , Toast.LENGTH_SHORT).show()
+//            }
+//       }
+        //------------------------------------------------------------------------------------
     }
 
     fun createBirthdayCard(view: View) {
-        val name = editTextPersonName.editableText.toString()
+        val name = binding.editTextPersonName.editableText.toString()
         Toast.makeText(this, "Name is $name" , Toast.LENGTH_SHORT).show()
     }
 }
